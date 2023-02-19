f2cee9bf1131a48e1c5c702f6a779b3246f53f44
Abdoul-99 <124527439+Abdoul-99@users.noreply.github.com>
2023-02-19 23:20:57 +0100

    Delete Js02 directory

--- Js02/addToArray.js
+++
@@ -1,3 +0,0 @@
-const myArray = [["John", 23],["Cat", 2]];
-myArray.push["Cs", 1];
-console.log(myArray[0,1,2]);
--- Js02/myName.js
+++
@@ -1,3 +0,0 @@
-let myName = "Daouda";
-let myStr = "My name is" + " " + myName + " " + "and I am well"; 
-console.log(myStr);
