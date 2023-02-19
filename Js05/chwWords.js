4a4d0cae932102404e2921ddc1b06a90d2e03340
Abdoul-99 <124527439+Abdoul-99@users.noreply.github.com>
2023-02-19 23:22:24 +0100

    Delete Js05 directory

--- Js05/chwWords.js
+++
@@ -1,9 +0,0 @@
-function chowWords() {
-  let mots = ["Niger", "Niamey", "Sadore", "Codeloccol" ];
-  for (let i= 0; i<mots.length; i++) {
-    console.log(mots[i]);
-  }
-  
-}
-chowWords();
-module.exports = chowWords; 
