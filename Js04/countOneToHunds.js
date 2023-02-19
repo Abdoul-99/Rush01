78ed3b4aa6527f87b3443c663c3eeaadfb90cebc
Abdoul-99 <124527439+Abdoul-99@users.noreply.github.com>
2023-02-19 23:21:56 +0100

    Delete Js04 directory

--- Js04/countOneToHunds.js
+++
@@ -1,7 +0,0 @@
-function countOneToHunds() {
-  for (i=1; i<=100; i++){
-    console.log(i)
-  }
-}
- countOneToHunds();
-module.exports = countOneToHunds;
--- Js04/multiply.js
+++
@@ -1,6 +0,0 @@
-function multiply(a,z) {
-  return a*z;
-}
-  console.log(multiply(5,10));
-
-module.exports = multiply;
