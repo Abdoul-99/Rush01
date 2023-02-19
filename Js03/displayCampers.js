ee21c036b0c18ca458857019681aa90c1d2629a4
Abdoul-99 <124527439+Abdoul-99@users.noreply.github.com>
2023-02-19 23:21:33 +0100

    Delete Js03 directory

--- Js03/displayCampers.js
+++
@@ -1,9 +0,0 @@
-const codeloccol = {  
-  name: 'coder',  
-  legs: 4,  
-  tails: 1,   
-  friends: ['Codeloccol Campers'], }; 
-  function Campers(){
-    console.log(codeloccol.friends);
-  }
-  Campers();
--- Js03/displayPersonInfo.js
+++
@@ -1,9 +0,0 @@
-let personne = {
-  "nom" : "Mariam",
-  "Age" : 30,
-};
-function displayPersonInfo () {
-  console.log("nom: Mariam");
-  console.log("Age: 30");
-};
-displayPersonInfo();
